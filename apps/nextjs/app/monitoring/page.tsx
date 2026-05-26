b5eab97a7b3144230528e88fb751ed7e53c921bd
AnejLaptopLinux <anej.eposta@gmail.com>
2026-05-26 12:51:41 +0200

    minor DockerFile adjustments and monitoring page.tsx <iframe> fixes

--- apps/nextjs/app/monitoring/page.tsx
+++ apps/nextjs/app/monitoring/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-
 export default function MonitoringPage() {
     return (
         <div className="-m-6 h-[calc(100vh-3.5rem)]">
             <iframe
-                src="http://localhost:3000/d/adv7vjj/sensum-digital-twin?orgId=1&kiosk&theme=dark"
+                src="http://localhost:3000/d/adv7vjj/sensum-digital-twin?orgId=1&kiosk&theme=dark&from=2026-01-01T00:00:00.000Z&to=2026-12-31T23:59:59.000Z"
                 className="w-full h-full border-0"
                 title="Sensum Digital Twin Dashboard"
             />
